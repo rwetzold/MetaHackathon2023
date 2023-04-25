886660cf1235220b0969d065165fecaa6ac4413e
João Lucas <jony.lucas.br13@gmail.com>
2023-04-25 15:17:40 -0300

    Update UnitBehaviour.cs

--- Assets/_Game/Scripts/Units/MonoBehaviour/UnitBehaviour.cs
+++ Assets/_Game/Scripts/Units/MonoBehaviour/UnitBehaviour.cs
@@ -4,4 +4,6 @@ public abstract class UnitBehaviour : MonoBehaviour
 {
     [SerializeField] private UnitAttributes _attributes;
     public GameObject ownerPlayer;
+
+    public abstract UnitAttributes Atrributes { get; }
 }
