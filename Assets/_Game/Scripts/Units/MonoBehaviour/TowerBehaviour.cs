93fd1bbca9d1a384313babc36206ad748c9f310f
Clarens <grollmann@me.com>
2023-04-27 10:19:07 +0200

    Update TowerBehaviour.cs

--- Assets/_Game/Scripts/Units/MonoBehaviour/TowerBehaviour.cs
+++ Assets/_Game/Scripts/Units/MonoBehaviour/TowerBehaviour.cs
@@ -14,6 +14,8 @@ namespace Hackathon.Commands
         [SerializeField]
         private GameObject _muzzleFlashes;
 
+        private bool _towerActiv = false;
+
         private AimTargetCommand _aimTarget = null;
         private float _lastShot = 0f;
         private float _muzzleFlashesTimer;
@@ -28,6 +30,11 @@ namespace Hackathon.Commands
             }
         }
 
+        public void SetTowerActiv()
+        {
+            _towerActiv = true;
+        }
+
         private void Start()
         {
             _lastShot = Time.time;
@@ -35,28 +42,31 @@ namespace Hackathon.Commands
 
         private void Update()
         {
-            if (_muzzleFlashesTimer < Time.time)
-                _muzzleFlashes.SetActive(false);
-            if (_aimTarget == null)
-                _aimTarget = new AimTargetCommand(this);
-            _aimTarget.Execute();
-
-            if (_aimTarget.nextTarget != null)
+            if (_towerActiv)
             {
-                Vector3 oldRotation = _cannonRotator.eulerAngles;
-                _cannonRotator.LookAt(_aimTarget.nextTarget.transform);
-                _cannonRotator.eulerAngles = new Vector3(_cannonRotator.eulerAngles.x, oldRotation.y, oldRotation.z);
+                if (_muzzleFlashesTimer < Time.time)
+                    _muzzleFlashes.SetActive(false);
+                if (_aimTarget == null)
+                    _aimTarget = new AimTargetCommand(this);
+                _aimTarget.Execute();
 
-                oldRotation = _turret.eulerAngles;
-                _turret.LookAt(_aimTarget.nextTarget.transform);
-                _turret.eulerAngles = new Vector3(oldRotation.x, _turret.eulerAngles.y + 180, oldRotation.z);
-
-                if (Time.time - _lastShot > armedAttributes.FireRageValue)
+                if (_aimTarget.nextTarget != null)
                 {
-                    _muzzleFlashes.SetActive(true);
-                    new AttacCommand(this, _aimTarget.nextTarget).Execute();
-                    _lastShot = Time.time;
-                    _muzzleFlashesTimer = Time.time + 0.1f;
+                    Vector3 oldRotation = _cannonRotator.eulerAngles;
+                    _cannonRotator.LookAt(_aimTarget.nextTarget.transform);
+                    _cannonRotator.eulerAngles = new Vector3(_cannonRotator.eulerAngles.x, oldRotation.y, oldRotation.z);
+
+                    oldRotation = _turret.eulerAngles;
+                    _turret.LookAt(_aimTarget.nextTarget.transform);
+                    _turret.eulerAngles = new Vector3(oldRotation.x, _turret.eulerAngles.y + 180, oldRotation.z);
+
+                    if (Time.time - _lastShot > armedAttributes.FireRageValue)
+                    {
+                        _muzzleFlashes.SetActive(true);
+                        new AttacCommand(this, _aimTarget.nextTarget).Execute();
+                        _lastShot = Time.time;
+                        _muzzleFlashesTimer = Time.time + 0.1f;
+                    }
                 }
             }
         }
