bdb5d837540e8ba1fd470fd6467a22c452375ac8
Robert Wetzold <rwetzold@gmail.com>
2023-04-27 10:05:56 +0100

    Tower outline toggle

--- Assets/_Game/Scripts/Units/MonoBehaviour/TowerBehaviour.cs
+++ Assets/_Game/Scripts/Units/MonoBehaviour/TowerBehaviour.cs
@@ -1,28 +1,27 @@
+using System.Linq;
 using Oculus.Interaction.HandGrab;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 namespace Hackathon.Commands
 {
     public class TowerBehaviour : UnitBehaviour
     {
-        [SerializeField]
-        private HandGrabInteractable _interactable, _inverseInteractable;
+        [SerializeField] private HandGrabInteractable _interactable, _inverseInteractable;
 
-        [SerializeField]
-        private Transform _cannonRotator;
+        [SerializeField] private Transform _cannonRotator;
 
-        [SerializeField]
-        private Transform _turret;
+        [SerializeField] private Transform _turret;
 
-        [SerializeField]
-        private GameObject _muzzleFlashes;
+        [SerializeField] private GameObject _muzzleFlashes;
+
+        [SerializeField] private GameObject[] _outlineGOs;
 
         private bool _towerActiv = false;
 
         private AimTargetCommand _aimTarget = null;
         private float _lastShot = 0f;
         private float _muzzleFlashesTimer;
+
         public ArmedUnitAttributes armedAttributes
         {
             get
@@ -37,6 +36,18 @@ namespace Hackathon.Commands
         public void SetTowerActiv()
         {
             _towerActiv = true;
+        }
+
+        [ContextMenu("Set Outlined")]
+        public void SetOutlined()
+        {
+            _outlineGOs.ToList().ForEach(go => go.SetActive(true));
+        }
+
+        [ContextMenu("Set Non Outlined")]
+        public void SetNonOutlined()
+        {
+            _outlineGOs.ToList().ForEach(go => go.SetActive(false));
         }
 
         private void Start()
@@ -58,7 +69,8 @@ namespace Hackathon.Commands
                 {
                     Vector3 oldRotation = _cannonRotator.eulerAngles;
                     _cannonRotator.LookAt(_aimTarget.nextTarget.transform);
-                    _cannonRotator.eulerAngles = new Vector3(_cannonRotator.eulerAngles.x, oldRotation.y, oldRotation.z);
+                    _cannonRotator.eulerAngles =
+                        new Vector3(_cannonRotator.eulerAngles.x, oldRotation.y, oldRotation.z);
 
                     oldRotation = _turret.eulerAngles;
                     _turret.LookAt(_aimTarget.nextTarget.transform);
